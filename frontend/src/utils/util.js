86b4fbe818e2f714b7d4b6cf5f5a7a88cdb342c3
Noerralfii <163947747+Noerralfii@users.noreply.github.com>
2025-05-27 21:08:36 +0700

    Update util.js

--- frontend/src/utils/util.js
+++ frontend/src/utils/util.js
@@ -1,2 +1,2 @@
 export const BASE_URL = "http://localhost:5000/api";
-export const BASE_URL_API = "https://notes-be183-651881126563.us-central1.run.app/api"
+export const BASE_URL_API = "https://notes-be183-427042664362.us-central1.run.app"
