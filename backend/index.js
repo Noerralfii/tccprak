6b888d9dde000c04ae740324de2e627a68b8cd1f
Noerralfii <163947747+Noerralfii@users.noreply.github.com>
2025-05-27 18:56:29 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // ✅ Konfigurasi CORS
 const allowedOrigins = [
+  "http://localhost:3000",
   "https://notes-fe183-dot-f-01-450707.uc.r.appspot.com"
 ];
 
